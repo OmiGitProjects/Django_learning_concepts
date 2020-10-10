594d972e3afbee50fcdb34a1227ca8aae501cefd
Onkar Nardekar <66405363+OmiGitProjects@users.noreply.github.com>
2020-10-11 01:28:46 +0530

    Update settings.py

--- django_form_manually_render/form_pro/settings.py
+++ django_form_manually_render/form_pro/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ocock=yy(v3739g=cuf02n8v3#1ymjt9ye3!th-o^y&@y*r6%5'
+# SECURITY WARNING: keep the secret key used in production secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
