e5d250bc38c218ce54d76d7a76ce955d0431a41a
Onkar Nardekar <66405363+OmiGitProjects@users.noreply.github.com>
2020-10-11 01:29:26 +0530

    Update settings.py

--- django_forms_AUTO_ID/form_manage/settings.py
+++ django_forms_AUTO_ID/form_manage/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-kw78nn_^-tkev8pn^v3*vm(j60u%k0^@6ip*vc4l$#649*#!s'
+# SECURITY WARNING: keep the secret key used in production secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
