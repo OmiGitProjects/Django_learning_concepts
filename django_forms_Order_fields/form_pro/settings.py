2b02cd7c4db64210f3557b6ade53dc04cbe2e903
Onkar Nardekar <66405363+OmiGitProjects@users.noreply.github.com>
2020-10-11 01:29:53 +0530

    Update settings.py

--- django_forms_Order_fields/form_pro/settings.py
+++ django_forms_Order_fields/form_pro/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+%9sjalu0398$(d-kua%jb7lndf89jdc1yz)mu76xzgm0kscy5'
+# SECURITY WARNING: keep the secret key used in production secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
